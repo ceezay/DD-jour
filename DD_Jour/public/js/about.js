9397fa9d2a5e189b498724db23fa553c989eaa52
Chinmay Jena <chinmay.jena.cdj@gmail.com>
2018-06-20 10:09:41 +0530

    minor modification

--- DD_Jour/public/js/about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  $('.profile').waypoint(function(direction) {
-     $('.profile').css('visibility', 'visible');
-    $('.profile').addClass('animated fadeInUp');
-  }, {
-    offset: '40%'
-  });
-});
